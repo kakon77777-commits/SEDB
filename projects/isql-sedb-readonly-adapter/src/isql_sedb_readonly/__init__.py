a57286f1698b9ae615515e7edf00c4d25d80d6fe
許筌崴(Neo.K) <kakon77777@gmail.com>
2026-09-14 19:03:53 +0800

    feat: export read-only ISQL SEDB adapter

---
+++ projects/isql-sedb-readonly-adapter/src/isql_sedb_readonly/__init__.py
@@ -0,0 +1,21 @@
+from .adapter import (
+    SEDB_ENTITY_SNAPSHOT_SCHEMA,
+    SEDBReadOnlyAdapterError,
+    SEDBExactStateMismatch,
+    SEDBFieldBindingSnapshot,
+    SEDBCellSnapshot,
+    SEDBEntitySnapshot,
+    VerifiedSEDBRead,
+    SEDBReadOnlyAdapter,
+)
+
+__all__ = [
+    "SEDB_ENTITY_SNAPSHOT_SCHEMA",
+    "SEDBReadOnlyAdapterError",
+    "SEDBExactStateMismatch",
+    "SEDBFieldBindingSnapshot",
+    "SEDBCellSnapshot",
+    "SEDBEntitySnapshot",
+    "VerifiedSEDBRead",
+    "SEDBReadOnlyAdapter",
+]
